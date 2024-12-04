a90edf926399ea5b0117b5d139df40fc8cc48ed0
sriveraar <phognus@hotmail.com>
2024-12-04 18:41:06 -0300

    add componentes
    se agrega carpeta componentes dentro un index y css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Header from "./componentes/Header/Header";
 import Formulario from './componentes/Formulario/Formulario';
 import MiOrg from './componentes/MiOrg';
+import Equipo from './componentes/Equipo';
 
 function App() {
-  const [mostrarFormulario, actualizarMostrar] = useState(true)
+  const [mostrarFormulario, actualizarMostrar] = useState(false)
 
   // Terbarui --> condicion ? seMuestra : noSeMuestra
   // condición && seMuestra
@@ -14,12 +15,65 @@ function App() {
     actualizarMostrar(!mostrarFormulario)
   }
 
+  // Lista de equipos
+  const equipos = [
+
+    {
+      titulo: "Programación",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9"
+    },
+
+    {
+      titulo: "Front-End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    }
+    ,
+
+    {
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    }
+    ,
+
+    {
+      titulo: "DevOps",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    },
+
+    {
+      titulo: "Ux y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
+    },
+
+    {
+      titulo: "Móvil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    },
+
+    {
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    }
+]
+
   return (
     <div>
       <Header />
       {/* { mostrarFormulario === true ? <Formulario /> : <div></div>} */}
       {mostrarFormulario && <Formulario />}
       <MiOrg cambiarMostrar={cambiarMostrar} />
+
+      {
+        equipos.map( (equipo) => <Equipo datos={equipo} key={equipo.titulo} />)
+      }
+
     </div>
   );
 }
---
+++ src/componentes/Equipo/index.js
@@ -0,0 +1,19 @@
+import "./Equipo.css"
+
+const Equipo = (props) => {
+    // Destructuración
+    const { colorPrimario, colorSecundario, titulo } = props.datos
+
+    const obj = {
+        backgroundColor: colorSecundario
+    }
+
+    const estiloTitulo = { borderColor: colorPrimario}
+    
+    return <section className="equipo" style={obj}> 
+        <h3 style={estiloTitulo}>{titulo}</h3>
+        <div className="colaboradores"></div>
+    </section>
+}
+
+export default Equipo
